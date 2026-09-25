7e52adb300294a42b604f46ce781b040e3eee5ff
Ngoc <huynhnhan254@gmail.com>
2026-09-25 16:26:40 -0500

    Completed 04_sumAll

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (!Number.isInteger(first) || !Number.isInteger(last) || first < 0 || last < 0) {
+        return "ERROR";
+    }
+
+    if (first > last) {
+        const temp = first;
+        first = last;
+        last = temp;
+    }
+
+    let res = 0;
+    for (let i = first; i <= last; i++) {
+        res += i;
+    }
+
+    return res;
 
 };
 
